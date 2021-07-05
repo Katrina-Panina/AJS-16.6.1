b1e69f5785d0b78e34e00fa587bc0a5c31a7a918
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-07-05 18:24:20 +0300

    fix

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,18 +1,41 @@
-import orderByProps from '../task';
+import sortObj from '../app';
 
-const obj = { name: 'мечник', health: 10, level: 2, attack: 80, defence: 40 };
+const testObj = {
+  name: 'мечник',
+  health: 10,
+  level: 2,
+  attack: 80,
+  defence: 40,
+};
 
-test('The function works if you do not pass the props', () => {
-  expect(() => orderByProps(obj)).not.toThrow();
+test('without rules, alphabet sort', () => {
+  expect(sortObj(testObj))
+    .toEqual([{ key: 'attack', value: 80 },
+      { key: 'defence', value: 40 },
+      { key: 'health', value: 10 },
+      { key: 'level', value: 2 },
+      { key: 'name', value: 'мечник' }]);
 });
 
-test('The function returns the correct result', () => {
-  const template = [
-    { key: 'name', value: 'мечник' },
-    { key: 'level', value: 2 },
-    { key: 'attack', value: 80 },
-    { key: 'defence', value: 40 },
-    { key: 'health', value: 10 },
-  ];
-  expect(orderByProps(obj, ['name', 'level'])).toEqual(template);
+test('2 rules, alphabet sort', () => {
+  expect(sortObj(testObj, ['level', 'name']))
+    .toEqual([{ key: 'level', value: 2 },
+      { key: 'name', value: 'мечник' },
+      { key: 'attack', value: 80 },
+      { key: 'defence', value: 40 },
+      { key: 'health', value: 10 }]);
+});
+
+test('All rules', () => {
+  expect(sortObj(testObj, ['level', 'defence', 'health', 'name', 'attack']))
+    .toEqual([{ key: 'level', value: 2 },
+      { key: 'defence', value: 40 },
+      { key: 'health', value: 10 },
+      { key: 'name', value: 'мечник' },
+      { key: 'attack', value: 80 }]);
+});
+
+test('Mistake in rules', () => {
+  expect(() => sortObj(testObj, ['level', 'defen']))
+    .toThrow();
 });
